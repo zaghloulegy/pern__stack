9db42a928c17d82083ad4900efcdb7f00a4ee60c
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-15 17:38:57 +0100

    almost done

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+// components
+
+import InputTodo from "./components/inputTodo";
+import ListTodos from "./components/listTodo";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+        <ListTodos />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
